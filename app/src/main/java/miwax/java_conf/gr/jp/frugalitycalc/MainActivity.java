bcfb4a3556d3968ed5631944a211328956299147
Tomoya.Miwa <tomoyann@gmail.com>
2016-02-16 19:52:44 +0900

    Implement MainActivity

--- app/src/main/java/miwax/java_conf/gr/jp/frugalitycalc/MainActivity.java
+++ app/src/main/java/miwax/java_conf/gr/jp/frugalitycalc/MainActivity.java
@@ -1,13 +1,118 @@
 package miwax.java_conf.gr.jp.frugalitycalc;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import miwax.java_conf.gr.jp.frugalitycalc.model.CalcNumber;
+import miwax.java_conf.gr.jp.frugalitycalc.model.Display;
+import miwax.java_conf.gr.jp.frugalitycalc.model.Operation;
+import miwax.java_conf.gr.jp.frugalitycalc.model.StateContext;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.result_text) TextView resultText;
+
+    private StateContext context = new StateContext();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        Display display = new Display(resultText);
+        context.setDisplay(display);
+    }
+
+    @OnClick(R.id.zero_button)
+    public void ZeroButton() {
+        context.onInputNumber(CalcNumber.ZERO);
+    }
+
+    @OnClick(R.id.one_button)
+    public void OneButton() {
+        context.onInputNumber(CalcNumber.ONE);
+    }
+
+    @OnClick(R.id.two_button)
+    public void TwoButton() {
+        context.onInputNumber(CalcNumber.TWO);
+    }
+
+    @OnClick(R.id.three_button)
+    public void ThreeButton() {
+        context.onInputNumber(CalcNumber.THREE);
+    }
+
+    @OnClick(R.id.four_button)
+    public void FourButton() {
+        context.onInputNumber(CalcNumber.FOUR);
+    }
+
+    @OnClick(R.id.five_button)
+    public void FiveButton() {
+        context.onInputNumber(CalcNumber.FIVE);
+    }
+
+    @OnClick(R.id.six_button)
+    public void SixButton() {
+        context.onInputNumber(CalcNumber.SIX);
+    }
+
+    @OnClick(R.id.seven_button)
+    public void SevenButton() {
+        context.onInputNumber(CalcNumber.SEVEN);
+    }
+
+    @OnClick(R.id.eight_button)
+    public void EightButton() {
+        context.onInputNumber(CalcNumber.EIGHT);
+    }
+
+    @OnClick(R.id.nine_button)
+    public void NineButton() {
+        context.onInputNumber(CalcNumber.NINE);
+    }
+
+    @OnClick(R.id.dot_button)
+    public void DotButton() {
+        context.onInputNumber(CalcNumber.DOT);
+    }
+
+    @OnClick(R.id.clearend_button)
+    public void ClearEndButton() {
+        context.onInputClearEnd();
+    }
+
+    @OnClick(R.id.allclear_button)
+    public void AllClearButton() {
+        context.onInputAllClear();
+    }
+
+    @OnClick(R.id.add_button)
+    public void AddButton() {
+        context.onInputOperator(Operation.PLUS);
+    }
+
+    @OnClick(R.id.sub_button)
+    public void SubButton() {
+        context.onInputOperator(Operation.MINUS);
+    }
+
+    @OnClick(R.id.multiple_button)
+    public void MultipleButton() {
+        context.onInputOperator(Operation.MULTIPLE);
+    }
+
+    @OnClick(R.id.div_button)
+    public void DivButton() {
+        context.onInputOperator(Operation.DIVIDE);
+    }
+
+    @OnClick(R.id.equal_button)
+    public void EqualButton() {
+        context.onInputEqual();
     }
 }
