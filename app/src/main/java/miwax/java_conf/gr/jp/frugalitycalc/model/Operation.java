1f797788c1e8230e260ef394fbf0a98278816992
Tomoya.Miwa <tomoyann@gmail.com>
2016-02-18 00:35:06 +0900

    Change the accuracy of the division to the IEEE 754R Decimal28

--- app/src/main/java/miwax/java_conf/gr/jp/frugalitycalc/model/Operation.java
+++ app/src/main/java/miwax/java_conf/gr/jp/frugalitycalc/model/Operation.java
@@ -1,12 +1,13 @@
 package miwax.java_conf.gr.jp.frugalitycalc.model;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 
 public enum Operation {
     PLUS     { BigDecimal apply(BigDecimal x, BigDecimal y) {return x.add(y);} },
     MINUS    { BigDecimal apply(BigDecimal x, BigDecimal y) {return x.subtract(y);} },
     MULTIPLE { BigDecimal apply(BigDecimal x, BigDecimal y) {return x.multiply(y);} },
-    DIVIDE   { BigDecimal apply(BigDecimal x, BigDecimal y) {return x.divide(y, 8, BigDecimal.ROUND_HALF_UP);} };
+    DIVIDE   { BigDecimal apply(BigDecimal x, BigDecimal y) {return x.divide(y, MathContext.DECIMAL128);} };
 
     abstract BigDecimal apply(BigDecimal x, BigDecimal y);
 }
