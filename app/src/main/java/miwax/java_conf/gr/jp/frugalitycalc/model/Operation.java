a735badc6f1f4ad2cef12afe814b5904a22dd58e
Tomoya.Miwa <tomoyann@gmail.com>
2016-02-24 23:38:21 +0900

    Add NONE to Operation and getString

--- app/src/main/java/miwax/java_conf/gr/jp/frugalitycalc/model/Operation.java
+++ app/src/main/java/miwax/java_conf/gr/jp/frugalitycalc/model/Operation.java
@@ -4,10 +4,21 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 
 public enum Operation {
-    PLUS     { BigDecimal apply(BigDecimal x, BigDecimal y) {return x.add(y);} },
-    MINUS    { BigDecimal apply(BigDecimal x, BigDecimal y) {return x.subtract(y);} },
-    MULTIPLE { BigDecimal apply(BigDecimal x, BigDecimal y) {return x.multiply(y);} },
-    DIVIDE   { BigDecimal apply(BigDecimal x, BigDecimal y) {return x.divide(y, MathContext.DECIMAL128);} };
+    NONE("")      { BigDecimal apply(BigDecimal x, BigDecimal y) {return new BigDecimal("0");} },
+    PLUS("+")     { BigDecimal apply(BigDecimal x, BigDecimal y) {return x.add(y);} },
+    MINUS("-")    { BigDecimal apply(BigDecimal x, BigDecimal y) {return x.subtract(y);} },
+    MULTIPLE("×") { BigDecimal apply(BigDecimal x, BigDecimal y) {return x.multiply(y);} },
+    DIVIDE("÷")   { BigDecimal apply(BigDecimal x, BigDecimal y) {return x.divide(y, MathContext.DECIMAL128);} };
+
+    private String string;
+
+    Operation(String s) {
+        this.string = s;
+    }
+
+    public String getString() {
+        return this.string;
+    }
 
     abstract BigDecimal apply(BigDecimal x, BigDecimal y);
 }
